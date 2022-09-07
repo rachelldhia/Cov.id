a4389c469bd7178b66e0782d9e419f1ff10d8dd3
nasywa zahra nailah <nasywazahranailah@gmail.com>
2022-09-07 08:39:26 +0700

    splashSreen and Protect

---
+++ app/src/main/java/com/rachel/covid/Protect.kt
@@ -0,0 +1,11 @@
+package com.rachel.covid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Protect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_protect)
+    }
+}
