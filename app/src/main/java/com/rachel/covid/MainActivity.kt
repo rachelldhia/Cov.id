bfff0f0c92a2f16ba2bc398462a7751e0e3e6bf2
Adeeva Rashida <rashidaadeeva@gmail.com>
2022-09-26 08:47:19 +0700

    array data

--- app/src/main/java/com/rachel/covid/MainActivity.kt
+++ app/src/main/java/com/rachel/covid/MainActivity.kt
@@ -3,10 +3,10 @@ package com.rachel.covid
 import android.annotation.SuppressLint
 import android.app.ProgressDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.rachel.covid.Model.Request
 import com.rachel.covid.databinding.ActivityMainBinding
@@ -15,15 +15,13 @@ import java.io.InputStreamReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
-class MainActivity : AppCompatActivity()
-{
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
-    var progressDialog: ProgressDialog? = null
+    lateinit var strProvince: Array<String>
 
 
-
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,33 +29,55 @@ class MainActivity : AppCompatActivity()
 
         fetchCovidData().start()
         provinceAndWays()
+
+        strProvince = resources.getStringArray(R.array.provinceName)
+        val spinner: Spinner = findViewById(R.id.spinnerProvinsi)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.provinceName,
+            R.layout.spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Toast.makeText( this@MainActivity, "Nothing Selected", Toast.LENGTH_SHORT).show()
+
+    }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val text: String = parent?.getItemAtPosition(position).toString()
     }
 
     @SuppressLint("SetTextI18n")
-    private fun fetchCovidData(): Thread
-    {
+    private fun fetchCovidData(): Thread {
+
+        val progressDialog = ProgressDialog(this@MainActivity)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Fetching data....")
+        progressDialog.show()
+
         return Thread {
             val url = URL("https://data.covid19.go.id/public/api/prov.json")
-            val connection  = url.openConnection() as HttpsURLConnection
-
-            if(connection.responseCode == 200)
-            {
+            val connection = url.openConnection() as HttpsURLConnection
+            if (connection.responseCode == 200) {
                 val inputSystem = connection.inputStream
                 val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
                 val request = Gson().fromJson(inputStreamReader, Request::class.java)
+
+                progressDialog.dismiss()
+
                 updateUI(request)
                 inputStreamReader.close()
                 inputSystem.close()
-            }
-            else
-            {
+            } else {
                 binding.connectionError.text = "Failed Connection"
             }
         }
     }
 
-    private fun updateUI(request: Request)
-    {
+    private fun updateUI(request: Request) {
         runOnUiThread {
             kotlin.run {
                 for (i in request.list_data.indices) {
@@ -74,14 +94,11 @@ class MainActivity : AppCompatActivity()
         }
     }
 
-
-
-    private fun provinceAndWays(){
+    private fun provinceAndWays() {
         val btnPrevent = findViewById<Button>(R.id.btn_prevent)
-        btnPrevent.setOnClickListener{
+        btnPrevent.setOnClickListener {
             startActivity(Intent(this, Protect::class.java))
         }
     }
-
 
 }
